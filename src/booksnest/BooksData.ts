cb891d698d88c7c39d1746ae33656bbbee5c85a9
HertelendyKristof <hertelendy.kristof@gmail.com>
2024-03-06 20:19:41 +0100

    Ossze raktam a feladatot csak nem commitoltam semmit eddig

---
+++ src/booksnest/BooksData.ts
@@ -0,0 +1,16 @@
+import { Book } from "./books";
+
+export const books: Book[] = [
+  {
+    id: '1',
+    title: 'A Kiválasztott',
+    author: 'Lois Lowry',
+    year: 1993,
+  },
+  {
+    id: '2',
+    title: 'Harry Potter és a Bölcsek Köve',
+    author: 'J.K. Rowling',
+    year: 1997,
+  },
+];
